33e92f53b7eda215ac5d6808aab8b88429db36b7
julilien <julianlienen@gmail.com>
2019-03-12 17:45:12 +0100

    Adds more TSBF tests and documentation

--- JAICore/jaicore-ml/test/jaicore/ml/tsc/classifier/trees/TimeSeriesBagOfFeaturesAlgorithmTest.java
+++ JAICore/jaicore-ml/test/jaicore/ml/tsc/classifier/trees/TimeSeriesBagOfFeaturesAlgorithmTest.java
@@ -13,8 +13,15 @@ import jaicore.basic.sets.SetUtil.Pair;
  */
 public class TimeSeriesBagOfFeaturesAlgorithmTest {
 
+	/**
+	 * Maximal delta for asserts with precision.
+	 */
 	private static final double EPS = 0.00001;
 
+	/**
+	 * See
+	 * {@link TimeSeriesBagOfFeaturesAlgorithm#discretizeProbs(int, double[][])}.
+	 */
 	@Test
 	public void discretizeProbsTest() {
 		int numBins = 10;
@@ -24,6 +31,10 @@ public class TimeSeriesBagOfFeaturesAlgorithmTest {
 				TimeSeriesBagOfFeaturesAlgorithm.discretizeProbs(numBins, probs)[0]);
 	}
 
+	/**
+	 * See
+	 * {@link TimeSeriesBagOfFeaturesAlgorithm#formHistogramsAndRelativeFreqs(int[][], int[], int, int, int)}.
+	 */
 	@Test
 	public void formHistogramsAndRelativeFreqsTest() {
 		int[][] discretizedProbs = new int[][] { { 1, 2, 3 } };
@@ -40,13 +51,16 @@ public class TimeSeriesBagOfFeaturesAlgorithmTest {
 		// Check histograms
 		Assert.assertArrayEquals(new int[] { 0, 1, 0, 0, 0 }, histograms[0][0]);
 		Assert.assertArrayEquals(new int[] { 0, 0, 1, 0, 0 }, histograms[0][1]);
-		Assert.assertArrayEquals(new int[] { 0, 0, 0, 1, 0 }, histograms[0][2]);
 
 		// Check relative freqs
 		Assert.assertArrayEquals(new int[] { 0, 0, 1 }, relativeFreqs[0]);
 
 	}
 
+	/**
+	 * See
+	 * {@link TimeSeriesBagOfFeaturesAlgorithm#generateHistogramInstances(int[][][], int[][])}.
+	 */
 	@Test
 	public void generateHistogramInstancesTest() {
 		int[][][] histograms = new int[][][] { { { 1, 0, 1 }, { 0, 1, 1 } } };
@@ -55,5 +69,65 @@ public class TimeSeriesBagOfFeaturesAlgorithmTest {
 		Assert.assertArrayEquals(new double[] { 1, 0, 1, 0, 1, 1, 0, 2 },
 				TimeSeriesBagOfFeaturesAlgorithm.generateHistogramInstances(histograms, relativeFreqsOfClasses)[0],
 				EPS);
+	}
+
+	/**
+	 * See
+	 * {@link TimeSeriesBagOfFeaturesAlgorithm#generateFeatures(double[][], int, int, int[][], int[][][])}.
+	 */
+	@Test
+	public void generateFeaturesTest() {
+		final double[][] data = new double[][] { { 1, 2, 3, 4 }, { 0, 0, 0, 0 } };
+		final int[][] subsequences = new int[][] { { 0, 3 } };
+		final int[][][] intervals = new int[][][] { { { 0, 2 } } };
+
+		double[][][][] generatedFeatures = TimeSeriesBagOfFeaturesAlgorithm.generateFeatures(data, subsequences,
+				intervals);
+
+		// Features of the first interval (first instance)
+		Assert.assertArrayEquals(new double[] { 1.5, 0.25, 1 }, generatedFeatures[0][0][0], EPS);
+
+		// Features of the first interval (last instance)
+		Assert.assertArrayEquals(new double[] { 0, 0, 0 }, generatedFeatures[1][0][0], EPS);
+
+		// Features of the subsequence (first instance)
+		Assert.assertArrayEquals(new double[] { 2, 0.66667, 1 }, generatedFeatures[0][0][1], EPS);
+
+		// Features of the subsequence (last instance)
+		Assert.assertArrayEquals(new double[] { 0, 0, 0 }, generatedFeatures[1][0][1], EPS);
+	}
+
+	/**
+	 * See
+	 * {@link TimeSeriesBagOfFeaturesAlgorithm#generateSubsequencesAndIntervals(int, int, int, int)}.
+	 */
+	@Test
+	public void generateSubsequencesAndIntervalsTest() {
+		final int minIntervalLength = 5;
+		
+		final TimeSeriesBagOfFeaturesAlgorithm algorithm = new TimeSeriesBagOfFeaturesAlgorithm(42, 10, 10, 0.1, minIntervalLength,
+				false);
+
+		final int r = 2;
+		final int d = 1;
+		final int T = 100;
+		final int lMin = 2;
+
+		final Pair<int[][], int[][][]> subsequencesAndIntervals = algorithm.generateSubsequencesAndIntervals(r, d, lMin,
+				T);
+		final int[][] subsequences = subsequencesAndIntervals.getX();
+		final int[][][] intervals = subsequencesAndIntervals.getY();
+
+		// Check subsequences dimensionality
+		Assert.assertEquals(r - d, subsequences.length);
+		Assert.assertEquals(2, subsequences[0].length);
+
+		// Check intervals dimensionality
+		Assert.assertEquals(r - d, intervals.length);
+		Assert.assertEquals(d, intervals[0].length);
+		
+		// Check interval values
+		Assert.assertTrue(intervals[0][0][1] - intervals[0][0][0] > minIntervalLength);
+		Assert.assertTrue(subsequences[0][1] - subsequences[0][0] > lMin);
 	}
 }
